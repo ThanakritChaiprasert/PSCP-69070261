7a63f2e15fdeaf7498f5558c53df11f59ddb3177
ThanakritChaiprasert <69070261@kmitl.ac.th>
2026-07-20 19:35:27 +0700

    Update learninglogs2.py

--- 2996/learninglogs2.py
+++ 2996/learninglogs2.py
@@ -1,11 +1,11 @@
 '''Colors'''
 color = input()
 color_mix = color + input()
-if color_mix == 'RedYellow' or color_mix == 'YellowRed':
+if color_mix in ['RedYellow', 'YellowRed']:
     print('Orange')
-elif color_mix == 'RedBlue' or color_mix == 'BlueRed':
+elif color_mix in ['RedBlue', 'BlueRed']:
     print('Purple')
-elif color_mix == 'YellowBlue' or color_mix == 'BlueYellow':
+elif color_mix in ['YellowBlue', 'BlueYellow']:
     print('Green')
 else:
     print('Error')
