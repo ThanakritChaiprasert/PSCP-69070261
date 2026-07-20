851afbebc45129126f2df18830bfcec46bfeb1b9
ThanakritChaiprasert <69070261@kmitl.ac.th>
2026-07-20 17:53:38 +0700

    Update submission6.py

--- 2996/submission6.py
+++ 2996/submission6.py
@@ -4,5 +4,5 @@ q2: float = input()
 p1: float = input()
 p2: float = input()
 
-EU_DISTANCE = .5 ** (((float(q1) - float(p1)) ** 2) + ((float(q2) - float(p2)) ** 2))
+EU_DISTANCE = ((float(q1) - float(p1)) ** 2 + (float(q2) - float(p2)) ** 2) ** 0.5
 print(EU_DISTANCE)
