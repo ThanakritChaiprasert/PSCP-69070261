1583e23c1da642c355ed4c8f9c6821d7127d33ed
ThanakritChaiprasert <69070261@kmitl.ac.th>
2026-07-20 18:46:00 +0700

    Create submission8.py

---
+++ 2996/submission8.py
@@ -0,0 +1,11 @@
+'''Heron of Alexandria'''
+a = float(input())
+b = float(input())
+c = float(input())
+
+if a >= 0 and b >= 0 and c >= 0:
+    s = (a+b+c)/2
+    area = (s*(s-a)*(s-b)*(s-c))**.5
+    print(area)
+else:
+    print("Invalid triangle")
