4f8b72d4db3eabb5528e5744e77a880681bc5f54
ThanakritChaiprasert <69070261@kmitl.ac.th>
2026-07-21 23:20:10 +0700

    Update 18_calculator.py

--- 2996/18_calculator.py
+++ 2996/18_calculator.py
@@ -5,7 +5,7 @@ D_COUNT = 2
 if int(key_press) <= 1:
     print(key_press)
 if int(key_press) > 1:
-    total = (int(key_press) * 2)
+    total = int(key_press) * 2
     while D_COUNT <= len(key_press):
         total = int(key_press) - ((10 ** (int(D_COUNT) - 1)) - 1) + int(total)
         D_COUNT += 1
