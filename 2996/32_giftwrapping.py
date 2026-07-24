f4ad668a1a404323ff3c1eedc785304de6822f9c
ThanakritChaiprasert <69070261@kmitl.ac.th>
2026-07-24 09:22:46 +0700

    Update 32_giftwrapping.py

--- 2996/32_giftwrapping.py
+++ 2996/32_giftwrapping.py
@@ -1,9 +1,9 @@
 '''Gift Wrapping'''
 PI = 3.14
-area = input()
-r = float(area[:area.index(' ')])
-h = float(area[area.index(' ') + 1:area.rindex(' ')])
-glue = float(area[area.rindex(' ') + 1:])
+area = input().split(' ')
+r = float(area[0])
+h = float(area[1])
+glue = float(area[2])
 
 WIDTH = h + (r * 2)
 LENGTH = (2 * PI * r) + glue
